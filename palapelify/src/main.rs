9998cba49607167b680d4d1099f7f337bcb8e9b9
Kristofer Rye <kristofer.rye@gmail.com>
2022-04-24 10:57:35 -0500

    palapelify: Extract GeoScalar type into lib/
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- palapelify/src/main.rs
+++ palapelify/src/main.rs
@@ -1,5 +1,7 @@
 // #![cfg_attr(debug_assertions, allow(dead_code))]
 
+use palapelify::GeoScalar;
+
 use std::{
 	collections::{BTreeMap, BTreeSet, HashMap, HashSet},
 	fs::{File, OpenOptions},
@@ -54,65 +56,6 @@ mod geoid {
 
 		let geoid: GeoId = string.clone().into();
 		assert_eq!(geoid.0, string);
-	}
-}
-
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct GeoScalar(i32);
-
-impl From<f64> for GeoScalar {
-	fn from(f64: f64) -> Self {
-		debug_assert!(f64 < 180.00 && f64 > -180.0);
-
-		Self((f64 * 1E6).trunc() as i32)
-	}
-}
-
-#[cfg(test)]
-mod geoscalar {
-	use super::GeoScalar;
-
-	#[cfg(test)]
-	mod from_f64 {
-		use super::GeoScalar;
-
-		impl core::fmt::Debug for GeoScalar {
-			fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-				f.debug_tuple("GeoScalar").field(&self.0).finish()
-			}
-		}
-
-		#[test]
-		fn converts_positive_in_range() {
-			let degrees: f64 = 87.31275;
-			assert_eq!(GeoScalar::from(degrees), GeoScalar(87312750));
-		}
-
-		#[test]
-		fn converts_negative_in_range() {
-			let degrees: f64 = -37.172718;
-			assert_eq!(GeoScalar::from(degrees), GeoScalar(-37172718));
-		}
-
-		#[test]
-		#[should_panic]
-		fn asserts_in_range_positive() {
-			let degrees: f64 = 180.0;
-			let _ = GeoScalar::from(degrees);
-		}
-
-		#[test]
-		#[should_panic]
-		fn asserts_in_range_negative() {
-			let degrees: f64 = -180.0;
-			let _ = GeoScalar::from(degrees);
-		}
-	}
-}
-
-impl From<GeoScalar> for f64 {
-	fn from(geo_scalar: GeoScalar) -> f64 {
-		(geo_scalar.0 as f64) / 1E6
 	}
 }
 
