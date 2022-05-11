671cff2e372cad14dd2536ae745fa73fe48f6d25
Kristofer Rye <kristofer.rye@gmail.com>
2022-05-10 19:57:35 -0500

    palapelify: Add some verbose tests for interner methods
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- palapelify/src/geoid.rs
+++ palapelify/src/geoid.rs
@@ -26,5 +26,98 @@ impl GeoIdInterner {
 	}
 }
 
+#[cfg(test)]
+mod interner {
+	use super::{GeoIdInterner, InternedGeoId};
+
+	#[cfg(test)]
+	mod default {
+		use super::GeoIdInterner;
+
+		#[test]
+		fn works() {
+			let _ = GeoIdInterner::default();
+		}
+	}
+
+	#[cfg(test)]
+	mod intern {
+		use string_interner::symbol::SymbolU32;
+
+		use super::{GeoIdInterner, InternedGeoId};
+
+		#[test]
+		fn intern_once() {
+			use string_interner::Symbol;
+			let mut interner = GeoIdInterner::default();
+			let id: InternedGeoId = interner.intern("181570111003007");
+			assert_eq!(
+				id,
+				InternedGeoId(SymbolU32::try_from_usize(0_usize).expect("0 is a valid index"))
+			);
+		}
+
+		#[test]
+		fn intern_twice_same_geoid() {
+			use string_interner::Symbol;
+			let mut interner = GeoIdInterner::default();
+			let id_first: InternedGeoId = interner.intern("181570111003007");
+			let id_second: InternedGeoId = interner.intern("181570111003007");
+			assert_eq!(
+				id_first,
+				InternedGeoId(SymbolU32::try_from_usize(0_usize).expect("0 is a valid index"))
+			);
+			assert_eq!(
+				id_second,
+				InternedGeoId(SymbolU32::try_from_usize(0_usize).expect("0 is a valid index"))
+			);
+			assert_eq!(id_first, id_second);
+			assert_eq!(
+				std::mem::size_of::<InternedGeoId>(),
+				std::mem::size_of::<u32>()
+			);
+		}
+
+		#[test]
+		fn intern_twice_same_geoid_and_once_more() {
+			use string_interner::Symbol;
+			let mut interner = GeoIdInterner::default();
+			let id_first: InternedGeoId = interner.intern("181570111003007");
+			let id_second: InternedGeoId = interner.intern("181570111003007");
+			let id_third: InternedGeoId = interner.intern("181570111003006");
+			assert_eq!(
+				id_first,
+				InternedGeoId(SymbolU32::try_from_usize(0_usize).expect("0 is a valid index"))
+			);
+			assert_eq!(
+				id_second,
+				InternedGeoId(SymbolU32::try_from_usize(0_usize).expect("0 is a valid index"))
+			);
+			assert_eq!(
+				id_third,
+				InternedGeoId(SymbolU32::try_from_usize(1_usize).expect("1 is a valid index"))
+			);
+			assert_eq!(id_first, id_second);
+			assert_eq!(
+				std::mem::size_of::<InternedGeoId>(),
+				std::mem::size_of::<u32>()
+			);
+		}
+	}
+
+	#[cfg(test)]
+	mod len {
+		use super::GeoIdInterner;
+
+		#[test]
+		fn default_zero() {
+			let interner = GeoIdInterner::default();
+			assert_eq!(interner.len(), 0_usize);
+		}
+	}
+}
+
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(test, derive(Debug))]
+#[repr(transparent)]
 pub struct InternedGeoId(SymbolU32);
