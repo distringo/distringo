e7ad91bdc59194e18cc5ecafb21a23f9bd061978
Kristofer Rye <kristofer.rye@gmail.com>
2022-07-23 13:37:51 -0500

    GeoId: Convert is_interned and is_raw to const fns
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- distringo/src/id.rs
+++ distringo/src/id.rs
@@ -13,14 +13,14 @@ impl From<String> for GeoId {
 }
 
 impl GeoId {
-	fn is_interned(&self) -> bool {
+	const fn is_interned(&self) -> bool {
 		match self {
 			Self::Interned(_) => true,
 			Self::Raw(_) => false,
 		}
 	}
 
-	fn is_raw(&self) -> bool {
+	const fn is_raw(&self) -> bool {
 		match self {
 			Self::Raw(_) => true,
 			Self::Interned(_) => false,
