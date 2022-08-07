6fdc6e67c080680bdb326868f78274d30d876be9
Kristofer Rye <kristofer.rye@gmail.com>
2022-08-07 08:08:52 -0500

    distringo: Split out GeoId into own submodule
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- distringo/src/id.rs
+++ distringo/src/id.rs
@@ -1,71 +1,15 @@
-use std::convert::Infallible;
+mod geoid;
+pub use geoid::*;
 
-#[derive(Clone, Debug)]
-enum GeoId {
-	Interned(u32),
-	Raw(Box<str>),
+struct GeoIdInterner {
+	names: std::collections::HashMap<Box<str>, u32>,
+	strings: Vec<Box<str>>,
 }
 
-impl From<String> for GeoId {
-	fn from(s: String) -> Self {
-		Self::Raw(s.into_boxed_str())
-	}
-}
+impl GeoIdInterner {
+	fn new() {}
 
-#[cfg(test)]
-mod from {
-	use super::GeoId;
-
-	#[cfg(test)]
-	mod string {
-		use super::GeoId;
-
-		#[test]
-		fn it_works() {
-			let string: String = String::from("hello, world!");
-			let result: GeoId = GeoId::from(string);
-			assert!(result.is_raw());
-		}
-	}
-}
-
-impl GeoId {
-	const fn is_interned(&self) -> bool {
-		match self {
-			Self::Interned(_) => true,
-			Self::Raw(_) => false,
-		}
-	}
-
-	const fn is_raw(&self) -> bool {
-		match self {
-			Self::Raw(_) => true,
-			Self::Interned(_) => false,
-		}
-	}
-}
-
-struct GeoIdInterner {}
-
-impl GeoIdInterner {}
-
-#[cfg(test)]
-mod test {
-	use super::GeoId;
-
-	#[test]
-	fn is_interned() {
-		let geoid = GeoId::Interned(0_u32);
-		assert!(geoid.is_interned());
-		let geoid = GeoId::Raw("".into());
-		assert!(!geoid.is_interned());
-	}
-
-	#[test]
-	fn is_raw() {
-		let geoid = GeoId::Interned(0_u32);
-		assert!(!geoid.is_raw());
-		let geoid = GeoId::Raw("".into());
-		assert!(geoid.is_raw());
+	fn get(&self, name: u32) -> Option<&Box<str>> {
+		self.strings.get(name as usize)
 	}
 }
---
+++ distringo/src/id/geoid.rs
@@ -0,0 +1,55 @@
+#[derive(Clone, Debug)]
+enum GeoId {
+	Interned(u32),
+	Raw(Box<str>),
+}
+
+impl From<String> for GeoId {
+	fn from(s: String) -> Self {
+		Self::Raw(s.into_boxed_str())
+	}
+}
+
+#[cfg(test)]
+mod from {
+	use super::GeoId;
+
+	#[test]
+	fn string() {
+		let string: String = String::from("hello, world!");
+		let result: GeoId = GeoId::from(string);
+		assert!(result.is_raw());
+	}
+}
+
+impl GeoId {
+	const fn is_interned(&self) -> bool {
+		match self {
+			Self::Interned(_) => true,
+			Self::Raw(_) => false,
+		}
+	}
+
+	const fn is_raw(&self) -> bool {
+		match self {
+			Self::Raw(_) => true,
+			Self::Interned(_) => false,
+		}
+	}
+}
+
+#[test]
+fn is_interned() {
+	let geoid = GeoId::Interned(0_u32);
+	assert!(geoid.is_interned());
+	let geoid = GeoId::Raw("".into());
+	assert!(!geoid.is_interned());
+}
+
+#[test]
+fn is_raw() {
+	let geoid = GeoId::Interned(0_u32);
+	assert!(!geoid.is_raw());
+	let geoid = GeoId::Raw("".into());
+	assert!(geoid.is_raw());
+}
