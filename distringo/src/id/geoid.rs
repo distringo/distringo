df0266c32397a2d2aebc4961902506d1969200e2
Kristofer Rye <kristofer.rye@gmail.com>
2022-08-07 12:07:08 -0500

    distringo: Rip out old GeoId type
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- distringo/src/id/geoid.rs
+++ distringo/src/id/geoid.rs
@@ -1,109 +1,46 @@
 #[derive(Clone, Copy)]
-#[cfg(feature = "new_geoid")]
 pub struct Interned(u32);
 
-#[cfg(feature = "new_geoid")]
 impl From<u32> for Interned {
 	fn from(u32: u32) -> Self {
 		Self(u32)
 	}
 }
 
-#[cfg(feature = "new_geoid")]
 impl From<Interned> for u32 {
 	fn from(interned: Interned) -> Self {
 		interned.0
 	}
 }
 
-#[cfg(feature = "new_geoid")]
+#[cfg(test)]
+mod raw {
+	use super::Raw;
+
+	#[cfg(test)]
+	mod from {
+		use super::Raw;
+
+		#[test]
+		fn string() {
+			let string: String = String::from("hello, world!");
+			let result: Raw = Raw::from(string.clone());
+			assert_eq!(result.0, string);
+		}
+	}
+}
+
 #[derive(Clone)]
 pub struct Raw(String);
 
-#[cfg(feature = "new_geoid")]
 impl From<String> for Raw {
 	fn from(s: String) -> Self {
 		Self(s)
 	}
 }
 
-#[cfg(feature = "new_geoid")]
 impl From<Raw> for String {
 	fn from(raw: Raw) -> Self {
 		raw.0
 	}
-}
-
-#[cfg(feature = "new_geoid")]
-impl core::ops::Deref for Raw {
-	type Target = String;
-
-	fn deref(&self) -> &Self::Target {
-		&self.0
-	}
-}
-
-#[derive(Clone, Debug)]
-#[cfg(not(feature = "new_geoid"))]
-#[deprecated]
-pub enum GeoId {
-	Interned(u32),
-	Raw(String),
-}
-
-#[cfg(not(feature = "new_geoid"))]
-impl From<String> for GeoId {
-	fn from(s: String) -> Self {
-		Self::Raw(s)
-	}
-}
-
-#[cfg(test)]
-#[cfg(not(feature = "new_geoid"))]
-mod from {
-	use super::GeoId;
-
-	#[test]
-	fn string() {
-		let string: String = String::from("hello, world!");
-		let result: GeoId = GeoId::from(string);
-		assert!(result.is_raw());
-	}
-}
-
-#[cfg(not(feature = "new_geoid"))]
-impl GeoId {
-	#[deprecated]
-	pub const fn is_interned(&self) -> bool {
-		match self {
-			Self::Interned(_) => true,
-			Self::Raw(_) => false,
-		}
-	}
-
-	#[deprecated]
-	pub const fn is_raw(&self) -> bool {
-		match self {
-			Self::Raw(_) => true,
-			Self::Interned(_) => false,
-		}
-	}
-}
-
-#[cfg(not(feature = "new_geoid"))]
-#[test]
-fn is_interned() {
-	let geoid = GeoId::Interned(0_u32);
-	assert!(geoid.is_interned());
-	let geoid = GeoId::Raw("".into());
-	assert!(!geoid.is_interned());
-}
-
-#[cfg(not(feature = "new_geoid"))]
-#[test]
-fn is_raw() {
-	let geoid = GeoId::Interned(0_u32);
-	assert!(!geoid.is_raw());
-	let geoid = GeoId::Raw("".into());
-	assert!(geoid.is_raw());
 }
--- distringo/src/id/interner.rs
+++ distringo/src/id/interner.rs
@@ -1,8 +1,5 @@
-#[cfg(not(feature = "new_geoid"))]
-pub use super::GeoId;
 pub use std::{borrow::Cow, collections::HashMap};
 
-#[cfg(feature = "new_geoid")]
 use crate::id::geoid::{Interned, Raw};
 
 #[derive(Default)]
@@ -20,33 +17,12 @@ impl<'i> GeoIdInterner<'i> {
 		self.strings.get(name as usize).is_some()
 	}
 
-	#[cfg(feature = "new_geoid")]
 	fn contains(&self, interned: Interned) -> bool {
 		self.contains_symbol(interned.into())
 	}
 
-	#[cfg(not(feature = "new_geoid"))]
-	fn contains_geoid(&self, geoid: GeoId) -> bool {
-		match geoid {
-			GeoId::Interned(name) => self.contains_symbol(name),
-			GeoId::Raw(_) => false,
-		}
-	}
-
-	#[cfg(feature = "new_geoid")]
 	pub fn intern(&mut self, raw: super::geoid::Raw) -> super::geoid::Interned {
-		self.intern_raw_string(raw.to_string()).into()
-	}
-
-	#[cfg(not(feature = "new_geoid"))]
-	pub fn intern(&mut self, geoid: GeoId) -> GeoId {
-		match geoid {
-			GeoId::Raw(str) => GeoId::Interned(self.intern_raw(&str)),
-			GeoId::Interned(name) => {
-				debug_assert!(self.contains_symbol(name));
-				geoid
-			}
-		}
+		self.intern_raw_string(raw.into()).into()
 	}
 
 	fn intern_raw_string(&mut self, string: String) -> u32 {
@@ -84,62 +60,31 @@ impl<'i> GeoIdInterner<'i> {
 		self.get_entry(name).map(|str| str.as_ref())
 	}
 
-	#[cfg(not(feature = "new_geoid"))]
-	fn get_interned_str(&self, geoid: &GeoId) -> Option<&str> {
-		match geoid {
-			GeoId::Interned(name) => self.get_str_raw(*name),
-			// TODO: This could be smarter? Isn't a GeoId::Raw(String) -> Some(&str) conversion possible?
-			GeoId::Raw(_) => None,
-		}
-	}
-
 	fn get_string_raw(&self, name: u32) -> Option<String> {
 		self.get_cloned(name).map(String::from)
 	}
 
-	#[cfg(not(feature = "new_geoid"))]
-	fn get_string(&self, geoid: GeoId) -> Option<String> {
-		match geoid {
-			GeoId::Interned(name) => self.get_string_raw(name),
-			GeoId::Raw(string) => Some(string),
-		}
-	}
-
-	#[cfg(feature = "new_geoid")]
 	pub fn resolve(&self, interned: Interned) -> Option<Raw> {
 		self.get_string_raw(interned.into()).map(Into::into)
-	}
-
-	#[cfg(not(feature = "new_geoid"))]
-	pub fn resolve(&self, geoid: GeoId) -> Option<GeoId> {
-		match geoid {
-			GeoId::Raw(ref _str) => Some(geoid),
-			GeoId::Interned(name) => self.get_string_raw(name).map(GeoId::Raw),
-		}
 	}
 }
 
 #[test]
-#[cfg(not(feature = "new_geoid"))]
 fn intern_and_get() {
 	// Start with a basic string
 	let string = String::from("a string");
 	// Convert that into a geoid
-	let geoid = GeoId::from(string);
-	// It should be raw at this point.
-	assert!(geoid.is_raw());
+	let raw = Raw::from(string.clone());
 
 	let mut interner: GeoIdInterner = GeoIdInterner::new();
-	let interned_geoid = interner.intern(geoid);
-	assert!(interned_geoid.is_interned());
+	let interned = interner.intern(raw);
 
-	let raw_geoid = interner.resolve(interned_geoid);
-	assert!(raw_geoid.is_some());
-	assert!(raw_geoid.unwrap().is_raw());
+	let resolved = interner.resolve(interned);
+	assert!(resolved.is_some());
+	assert_eq!(String::from(resolved.unwrap()), string);
 }
 
 #[test]
-#[cfg(feature = "new_geoid")]
 fn intern_and_get_multiple() {
 	let mut interner = GeoIdInterner::new();
 
@@ -164,45 +109,12 @@ fn intern_and_get_multiple() {
 	}
 }
 
-#[test]
-#[cfg(not(feature = "new_geoid"))]
-fn intern_and_get_multiple() {
-	let mut interner = GeoIdInterner::new();
-
-	let geoid_0_0 = interner.intern(GeoId::from(String::from("a string")));
-	let geoid_0_1 = interner.intern(GeoId::from(String::from("a string")));
-	let geoid_1_0 = interner.intern(GeoId::from(String::from("another string")));
-	let geoid_0_2 = interner.intern(GeoId::from(String::from("a string")));
-	let geoid_1_1 = interner.intern(GeoId::from(String::from("another string")));
-
-	assert!(geoid_0_0.is_interned());
-	assert!(geoid_0_1.is_interned());
-	assert!(geoid_0_2.is_interned());
-	assert!(geoid_1_0.is_interned());
-	assert!(geoid_1_1.is_interned());
-
-	match ((geoid_0_0, geoid_0_1, geoid_0_2), (geoid_1_0, geoid_1_1)) {
-		(
-			(GeoId::Interned(name_0_0), GeoId::Interned(name_0_1), GeoId::Interned(name_0_2)),
-			(GeoId::Interned(name_1_0), GeoId::Interned(name_1_1)),
-		) => {
-			assert!(name_0_0 == name_0_1 && name_0_1 == name_0_2);
-			assert!(name_1_0 == name_1_1);
-		}
-		_ => unreachable!(),
-	}
-}
-
 #[cfg(test)]
 mod contains_symbol {
-	#[cfg(not(feature = "new_geoid"))]
-	use super::GeoId;
 	use super::GeoIdInterner;
-	#[cfg(feature = "new_geoid")]
 	use super::{Interned, Raw};
 
 	#[test]
-	#[cfg(feature = "new_geoid")]
 	fn inserted() {
 		let raw = Raw::from(String::from("a string"));
 
