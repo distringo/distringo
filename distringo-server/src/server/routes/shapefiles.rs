22e12cc15baf258b5e40fb1009a5e9884bb106f7
Kristofer Rye <kristofer.rye@gmail.com>
2022-10-23 10:20:49 -0500

    shapefiles: Start fleshing out lazy-loading infrastructure
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- distringo-server/src/server/routes/shapefiles.rs
+++ distringo-server/src/server/routes/shapefiles.rs
@@ -2,10 +2,57 @@ use std::collections::HashMap;
 
 use axum::{extract::Path, response::IntoResponse, routing::get, Json, Router};
 
+use serde::Deserialize;
+
 type ShapefileId = String;
 
 struct ShapefileDatabase {
-	entries: HashMap<ShapefileId, ()>,
+	entries: HashMap<ShapefileId, LazyFileContents>,
+}
+
+struct LazyFileContents {
+	file_handle: std::fs::File,
+	contents: Option<std::borrow::Cow<'static, str>>,
+}
+
+impl LazyFileContents {
+	fn try_from_file_name<P: AsRef<std::path::Path>>(file: P) -> std::io::Result<Self> {
+		let file_handle = std::fs::File::open(file)?;
+
+		Ok(Self {
+			file_handle,
+			contents: None,
+		})
+	}
+
+	async fn get_contents(&self) -> String {
+		if let Some(str) = &self.contents {
+			str.to_string()
+		} else {
+			todo!()
+		}
+	}
+}
+
+impl From<&crate::settings::ShapefilesConfig> for ShapefileDatabase {
+	#[tracing::instrument(skip_all)]
+	fn from(config: &crate::settings::ShapefilesConfig) -> Self {
+		tracing::trace!("initializing shapefile database");
+
+		let mut entries = HashMap::new();
+
+		let shapefiles: &HashMap<String, crate::settings::ShapefileConfig> = config.as_ref();
+
+		for (key, shapefile_config) in shapefiles {
+			// let config: ShapefileConfig = shapefile_config.parse();
+
+			tracing::debug!(id = key, "loading shapefile");
+
+			// entries.insert(key, todo!());
+		}
+
+		Self { entries }
+	}
 }
 
 impl ShapefileDatabase {
@@ -14,40 +61,25 @@ impl ShapefileDatabase {
 		Json(self.entries.keys().collect::<Vec<_>>())
 	}
 
-	#[tracing::instrument(skip(self))]
+	// #[tracing::instrument(skip(self))]
 	async fn show(&self, Path(id): Path<ShapefileId>) -> impl IntoResponse + '_ {
 		if let Some(shapefile) = self.entries.get(&id) {
-			tracing::debug!(?id, "Retrieved shapefile");
-			Ok("found shapefile {}")
+			tracing::debug!(?id, "shapefile found");
+
+			let contents: String = shapefile.get_contents().await;
+
+			Ok(contents)
 		} else {
-			Err("no such shapefile")
+			tracing::debug!(?id, "shapefile not found");
+			Err(http::StatusCode::NOT_FOUND)
 		}
 	}
 }
 
 pub(crate) fn router(config: &crate::settings::ShapefilesConfig) -> Router {
-	println!("{:?}", config);
-
-	let db = ShapefileDatabase {
-		entries: HashMap::new(),
-	};
-
-	tracing::trace!("initializing shapefile database");
+	let db: ShapefileDatabase = config.into();
 
 	let db = Box::leak(Box::new(db));
-
-	let shapefiles = config.get_table("shapefiles");
-
-	match shapefiles {
-		Ok(shapefiles) => {
-			for (key, shapefile_config) in shapefiles {
-				tracing::debug!(id = key, "loading shapefile from {:?}", shapefile_config);
-
-				db.entries.insert(key, ());
-			}
-		}
-		Err(err) => tracing::warn!(error = %err, "error getting shapefiles from configuration"),
-	};
 
 	Router::new()
 		.route("/", get(|| db.index()))
