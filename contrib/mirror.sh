6d0f7d634bd14baba8b086c6137fa6cda19724f3
Kristofer Rye <kristofer.rye@gmail.com>
2023-02-12 15:44:24 -0600

    Make mirror script do less
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- contrib/mirror.sh
+++ contrib/mirror.sh
@@ -5,11 +5,15 @@ year="$1"
 case "$year" in
 
 	"2010")
-		wget -m "ftp://ftp2.census.gov/geo/tiger/TIGER2010/"
+		# wget -m "ftp://ftp2.census.gov/geo/tiger/TIGER2010/"
+		wget -m "ftp://ftp2.census.gov/programs-surveys/decennial/2010/data/01-Redistricting_File--PL_94-171/Wisconsin"
+		# wget -m "ftp://ftp2.census.gov/programs-surveys/decennial/2010/data/01-Redistricting_File--PL_94-171"
 		;;
 
 	"2020")
 		wget -m "ftp://ftp2.census.gov/geo/tiger/TIGER2020PL/STATE/"
+		wget -m "ftp://ftp2.census.gov/programs-surveys/decennial/2020/data/01-Redistricting_File--PL_94-171"
+		# wget -m "https://www2.census.gov/programs-surveys/decennial/2020/data/01-Redistricting_File--PL_94-171/"
 		;;
 
 esac
