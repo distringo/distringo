d9666227d08878c874272cd6b379c6d9ad817c51
Kristofer Rye <kristofer.rye@gmail.com>
2023-03-22 08:04:53 -0500

    fetch: Refactor repl into Repl struct
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- distringo-fetch/src/main.rs
+++ distringo-fetch/src/main.rs
@@ -5,82 +5,83 @@ use tokio::io::AsyncBufReadExt;
 use tracing::{error, info, warn};
 
 #[derive(Default)]
-struct ReplContext {
+struct Repl {
 	banner_seen: bool,
 	history: Vec<String>,
 	exiting: bool,
 }
 
-async fn banner(ctx: &mut ReplContext) {
-	println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-	println!("");
+impl Repl {
+	async fn banner(&mut self) {
+		println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+		println!("");
 
-	ctx.banner_seen = true;
-}
-
-async fn menu(ctx: &mut ReplContext) {}
-
-async fn prompt(ctx: &mut ReplContext) {
-	use std::io::{stdout, Write};
-
-	if !ctx.banner_seen {
-		banner(ctx).await;
+		self.banner_seen = true;
 	}
 
-	menu(ctx).await;
+	async fn menu(&mut self) {}
 
-	print!("> ");
+	async fn prompt(&mut self) {
+		use std::io::{stdout, Write};
 
-	stdout().flush().expect("error!");
-}
+		if !self.banner_seen {
+			self.banner().await;
+		}
 
-async fn command(ctx: &mut ReplContext, input: &str) {
-	ctx.history.push(input.to_string());
+		self.menu().await;
 
-	match input.to_lowercase().trim() {
-		"exit" => ctx.exiting = true,
-		command => warn!("unknown command {command}"),
+		print!("> ");
+
+		stdout().flush().expect("error!");
 	}
-}
 
-async fn repl() {
-	let stdin = tokio::io::stdin();
-	let mut stdin = tokio::io::BufReader::new(stdin);
-	let mut prompt_ctx = ReplContext::default();
+	async fn command(&mut self, input: &str) {
+		self.history.push(input.to_string());
 
-	loop {
-		// Display a prompt.
-		prompt(&mut prompt_ctx).await;
+		match input.to_lowercase().trim() {
+			"exit" => self.exiting = true,
+			command => warn!("unknown command {command}"),
+		}
+	}
 
-		// Read a line of input.
-		let mut buffer = Vec::new();
-		let result = stdin.read_until(b'\n', &mut buffer).await;
+	async fn run(&mut self) {
+		let stdin = tokio::io::stdin();
+		let mut stdin = tokio::io::BufReader::new(stdin);
 
-		match result {
-			Ok(0) => {
-				print!("\r");
-				prompt_ctx.exiting = true;
-			}
-			Ok(bytes) => {
-				// TODO: Potential guardrail for length of input?
+		loop {
+			// Display a prompt.
+			self.prompt().await;
 
-				match String::from_utf8(buffer) {
-					Ok(input) => command(&mut prompt_ctx, &input).await,
-					Err(err) => error!(
-						?err,
-						"error converting {bytes} of input from utf8, ignoring"
-					),
+			// Read a line of input.
+			let mut buffer = Vec::new();
+			let result = stdin.read_until(b'\n', &mut buffer).await;
+
+			match result {
+				Ok(0) => {
+					print!("\r");
+					self.exiting = true;
 				}
+				Ok(bytes) => {
+					// TODO: Potential guardrail for length of input?
+
+					match String::from_utf8(buffer) {
+						Ok(input) => self.command(&input).await,
+						Err(err) => error!(
+							?err,
+							"error converting {bytes} of input from utf8, ignoring"
+						),
+					}
+				}
+				Err(err) => error!(?err, "error!"),
 			}
-			Err(err) => error!(?err, "error!"),
+
+			if self.exiting {
+				break;
+			}
 		}
 
-		if prompt_ctx.exiting {
-			break;
-		}
+		info!("exiting");
 	}
-
-	info!("exiting");
 }
 
 #[tokio::main]
@@ -90,7 +91,7 @@ async fn main() {
 	}
 
 	if atty::is(atty::Stream::Stdin) && atty::is(atty::Stream::Stdout) {
-		repl().await;
+		Repl::default().run().await;
 	} else {
 		println!("not sure what you want me to do here");
 	}
