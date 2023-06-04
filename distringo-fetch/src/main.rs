55e00303d8d32af57afa183b7b20df8f3b3d010a
Kristofer Rye <kristofer.rye@gmail.com>
2023-06-04 09:04:05 -0500

    fetch: Remove dependency on atty in favor of std impl
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- distringo-fetch/src/main.rs
+++ distringo-fetch/src/main.rs
@@ -1,5 +1,7 @@
 #![warn(rust_2018_idioms, future_incompatible)]
 
+use std::io::{stdin, stdout, IsTerminal};
+
 use tracing::error;
 
 mod repl;
@@ -11,7 +13,7 @@ async fn main() {
 		tracing_subscriber::fmt().init();
 	}
 
-	if atty::is(atty::Stream::Stdin) && atty::is(atty::Stream::Stdout) {
+	if stdin().is_terminal() && stdout().is_terminal() {
 		Repl::default().run().await;
 	} else {
 		error!("not sure what you want me to do here");
